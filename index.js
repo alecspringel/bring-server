d57b2e0429f797e20fcc83e1df3dd4e9b211015e
Sam Peters <sam@sl27.net>
2020-12-15 23:42:31 -0800

    Added basic rate limiting to api/donations and api/user/login, still need to tweak exact numbers

--- index.js
+++ index.js
@@ -26,10 +26,29 @@ app.use(
 );
 app.use(bodyParser.json());
 
+
+// Rate Limits
+const rateLimit = require("express-rate-limit");
+donationLimiter = rateLimit({
+  windowMs: 240 * 60 * 1000, // 4 hours
+  max: 15,
+  message: "Max donation api calls exceeded for this time period",
+});
+
+loginLimiter = rateLimit({
+  windowMs: 60 * 60 * 1000, // 60 minutes
+  max: 10,
+  message: "Max login api calls exceeded for this time period",
+});
+
+//app.use("/api/donations/create", donationLimiter);
+//app.use("/api/user", loginLimiter);
 // Endpoints
 app.use("/api/user", user);
 app.use("/api/donations", donations);
 app.use("/api/responses", responses);
+
+
 
 app.listen(PORT, () => {
   console.log(`Bring server running at http://localhost:${PORT}`);
